1c24768d915f45e84901d7bd6549114e9b4bee9b
Marcial Lincoln <marcial.snake@gmail.com>
2020-02-14 20:39:04 -0300

    IA do inimigo patrulhando na plataforma

---
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPatrol : MonoBehaviour {
+
+    public float speed;
+    public Transform groundCheck;
+    public LayerMask whatsIsGround;
+
+    private Rigidbody2D body;
+    private bool isGround;
+    private bool faceRight = true;
+
+
+
+    private void Start() {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update() {
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.02f, whatsIsGround);
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        if (!isGround)
+            Flip();
+    }
+
+
+    private void Flip() {
+        faceRight = !faceRight;
+
+        if (faceRight)
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        else
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+    }
+}
